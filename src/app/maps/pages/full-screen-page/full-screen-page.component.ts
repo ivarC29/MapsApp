f223af2da1ee2d2a81e5cf02dbc3b42b11e6e4f1
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-03-22 11:41:42 -0500

    Implement markers page and properties pages. (End)

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -16,7 +16,7 @@ export class FullScreenPageComponent implements AfterViewInit{
     const map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: [-77.03, -12.07], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
   }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from "mapbox-gl";
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from "mapbox-gl";
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent implements AfterViewInit{
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy{
 
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 9;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-77.03, -12.07);
 
   ngAfterViewInit(): void {
 
@@ -19,11 +20,15 @@ export class ZoomRangePageComponent implements AfterViewInit{
     this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   mapListeners() {
@@ -37,6 +42,11 @@ export class ZoomRangePageComponent implements AfterViewInit{
       if ( this.map!.getZoom() < 18 ) return;
       this.map!.zoomTo(18);
     });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+    })
+
   }
 
   zoomIn() {
